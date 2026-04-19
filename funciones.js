cdc89d4481166401e738809563e9e88a6797b1a9
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-19 10:33:49 -0500

    Se creo la funcion calcularDisponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,10 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+// funciones.js
+function calcularDisponible(ingresos, egresos){
+    let disponible = ingresos - egresos;
+    if(disponible < 0){
+        return 0;
+    }
+    return disponible;
+}
