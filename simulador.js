417c109dceb2e0c427845c1b7f07092d98f90b24
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-19 11:02:32 -0500

    Se calculo la capacidad de pago mensual

--- simulador.js
+++ simulador.js
@@ -1,11 +1,14 @@
 function calcular(){
-    // 1. Leer ingresos
     let ingresos = parseFloat(document.getElementById("txtIngresos").value);
-    // 2. Leer egresos
     let egresos = parseFloat(document.getElementById("txtEgresos").value);
-    // 3. Llamar a calcularDispoible
+
     let disponible = calcularDisponible(ingresos, egresos);
-    // 4. Mostrar en pantalla
-document.getElementById("spnDisponible").textContent = disponible.toFixed(2);}
-// evento botón
+
+    document.getElementById("spnDisponible").textContent = disponible.toFixed(2);
+
+    let capacidad = calcularCapacidadPago(disponible);
+
+    document.getElementById("spnCapacidadPago").textContent = capacidad.toFixed(2);
+}
+
 document.getElementById("btnCalcularCredito").onclick = calcular;
