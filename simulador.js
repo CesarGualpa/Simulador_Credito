cb8624d2129e7ca48f1a6745e974df542d8b14f4
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-19 11:24:13 -0500

    Se agrego calculo del total a pagar

--- simulador.js
+++ simulador.js
@@ -3,12 +3,20 @@ function calcular(){
     let egresos = parseFloat(document.getElementById("txtEgresos").value);
 
     let disponible = calcularDisponible(ingresos, egresos);
-
     document.getElementById("spnDisponible").textContent = disponible.toFixed(2);
 
     let capacidad = calcularCapacidadPago(disponible);
-
     document.getElementById("spnCapacidadPago").textContent = capacidad.toFixed(2);
+
+    let monto = parseInt(document.getElementById("txtMonto").value);
+    let plazoAnios = parseInt(document.getElementById("txtPlazo").value);
+    let tasa = parseInt(document.getElementById("txtTasaInteres").value);
+
+    let interes = calcularInteresSimple(monto, tasa, plazoAnios);
+    document.getElementById("spnInteresPagar").textContent = interes.toFixed(2);
+
+    let totalPagar = calcularTotalPagar(monto, interes);
+    document.getElementById("spnTotalPrestamo").textContent = totalPagar.toFixed(2);
 }
 
 document.getElementById("btnCalcularCredito").onclick = calcular;
