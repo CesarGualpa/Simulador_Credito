bc0b4c765eea624f004d1255b012ca5e764ed4ba
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-19 10:47:40 -0500

    Se creo la funcion calcular

--- simulador.js
+++ simulador.js
@@ -1,1 +1,12 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    // 1. Leer ingresos
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    // 2. Leer egresos
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+    // 3. Llamar a calcularDispoible
+    let disponible = calcularDisponible(ingresos, egresos);
+    // 4. Mostrar en pantalla
+document.getElementById("spnDisponible").textContent = disponible.toFixed(2);}
+// evento botón
+document.getElementById("btnCalcularCredito").onclick = calcular;
